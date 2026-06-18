2adf4f66011863097678202f2f8537d330daf672
charles_jay <17302732991@189.cn>
2026-06-18 12:04:29 +0800

    fix: 优化 OSS 文件层级结构
    - 年月放在一个目录（202606 而不是 2026/06）

--- app/Helpers/OssHelper.php
+++ app/Helpers/OssHelper.php
@@ -22,10 +22,10 @@ class OssHelper
 
         // 对于视频和文档，添加 courses/ 子目录
         if (in_array($type, ['video', 'document'])) {
-            return $prefix . '/' . $typeDir . '/courses/' . date('Y/m') . '/' . $fileName;
+            return $prefix . '/' . $typeDir . '/courses/' . date('Ym') . '/' . $fileName;
         }
 
-        return $prefix . '/' . $typeDir . '/' . date('Y/m') . '/' . $fileName;
+        return $prefix . '/' . $typeDir . '/' . date('Ym') . '/' . $fileName;
     }
 
     /**
